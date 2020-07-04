14f3dc441d2b1d4eb49a7a1652c69ef48e963fc3
Lokesh Senthil <66515225+lokeshsenthil@users.noreply.github.com>
2020-07-04 12:09:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+    playsound(buttonInnerHTML);
+  });
+}
+
+function playsound(key)
+{
+  key=key.toLowerCase();
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom1 = new Audio("sounds/tom-2.mp3");
+      tom1.play();
+      break;
+
+    case "s":
+      var tom1 = new Audio("sounds/tom-3.mp3");
+      tom1.play();
+      break;
+
+    case "d":
+      var tom1 = new Audio("sounds/tom-4.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      var tom1 = new Audio("sounds/snare.mp3");
+      tom1.play();
+      break;
+
+    case "k":
+      var tom1 = new Audio("sounds/crash.mp3");
+      tom1.play();
+      break;
+    case "l":
+      var tom1 = new Audio("sounds/kick-bass.mp3");
+      tom1.play();
+      break;
+    // default:console.log(buttonInnerHTML);
+  }
+}
+
+document.addEventListener("keydown",function(event){
+  playsound(event.key);
+});
